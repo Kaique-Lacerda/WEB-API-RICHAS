78308635b939a009c3edfdef36d55944a660d98e
Kaique Gabriel <149600790+Kaique-Lacerda@users.noreply.github.com>
2025-10-03 16:30:52 +0000

    AA

--- src/paginas/Repositorios/index.js
+++ src/paginas/Repositorios/index.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import estilos from './estilos';
-import { pegaRepositrioDoUsuario } from '../../servicos/requisicoes/repositorios';
+import { pegaRepositorioDoUsuario } from '../../servicos/requisicoes/repositorios';
 
 export default function Repositorios({ route, navigation }) {
     const [repo, setRepo] = useState([]);
 
     useEffect(async()=>{
-        const resultado = await pegaRepositrioDoUsuario(route.params.id)
+        const resultado = await pegaRepositorioDoUsuario(route.params.id)
         setRepo(resultado)
     },[])
 
@@ -28,6 +28,7 @@ export default function Repositorios({ route, navigation }) {
             renderItem={({item})=>(
                 <TouchableOpacity
                 style={estilos.repositorio}
+                onPress={() => navigation.navigate('InfoRepositorio', {item})}
                 >
                         <Text style={estilos.repositorioNome}>{item.name}</Text>
                         <Text style={estilos.repositorioData}>Atualizado em {item.data}</Text>
--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -1,6 +1,6 @@
 import api from "../api";
 
-export async function pegaRepositrioDoUsuario(id) {
+export async function pegaRepositorioDoUsuario(id) {
      try{
         const resultado =  await api.get(`/repos?postId=${id}`);
         return resultado.data
@@ -10,4 +10,43 @@ export async function pegaRepositrioDoUsuario(id) {
         return []
     }
     
+}
+
+export async function salvarRepositoriosDoUsuario(postId, nome, data, id) {
+    try{
+        await api.post(`/repos/${id}`, 
+            {
+            name: nome,
+            data: data,
+            postId: postId
+            }
+        );
+    }catch (error){
+        console.log("O erro é: "+error)
+        return 'erro'
+    }
+}
+
+export async function criarRepositorioDoUsuario(postId, nome, data) {
+    try{
+        await api.post(`/repos`, {
+            name: nome,
+            data: data,
+            postId: postId
+            });
+            return 'sucesso'
+    }catch (error){
+        console.log("O erro é: "+error)
+        return 'erro'
+    }
+}
+
+export async function deletarRepositorioDoUsuario(id) {
+    try{
+        await api.delete(`/repos/${id}`);
+            return 'sucesso'
+    }catch (error){
+        console.log("O erro é: "+error)
+        return 'erro'
+    }
 }
